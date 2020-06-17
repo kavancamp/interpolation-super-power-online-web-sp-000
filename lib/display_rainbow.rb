c97ca3b14ecf700aa748de7f7a97bd544d77c03a
Keenah Vancampenhout <kvancamp@mail.com>
2020-06-17 22:44:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
-def display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo",  "violet"])
+def display_rainbow
+  colors = ["red", "orange", "yellow", "green", "blue", "indigo",  "violet"]
 
 puts "R: #{colors[0]}"
 puts "O: #{colors[1]}"
